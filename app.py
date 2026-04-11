c56a444a269a6d49c4e09eb8a67c618717d31e28
bashen2006 <73512517+bashen2006@users.noreply.github.com>
2026-04-12 01:25:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -105,11 +105,11 @@ def get_stock_data(stock_code):
             ts_code = stock_code + ".SZ"
 
         df = ts.pro_bar(
-          save_cache(stock_code, df)
             ts_code=ts_code,
             adj='qfq',
             limit=100
         )
+      save_cache(stock_code, df)
 
         print("ts_code:", ts_code)
         print(df)
